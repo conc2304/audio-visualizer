0d961d4f99d72b9669574cc4899387d1020c4376
Jose Conchello <joseconchello@Joses-MacBook-Pro.local>
2019-10-29 20:40:27 -0400

    vyzby-app - add basic splash screen

--- vyzby-app/src/App.vue
+++ vyzby-app/src/App.vue
@@ -1,36 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar app>
-      <v-toolbar-title class="headline text-uppercase">
-        <span>Vuetify</span>
-        <span class="font-weight-light">MATERIAL DESIGN</span>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-btn
-        text
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-      >
-        <span class="mr-2">Latest Release</span>
-      </v-btn>
-    </v-app-bar>
-
     <v-content>
-      <HelloWorld/>
+      <HelloWorld v-if="location ==='home'"/>
+      <SplashScreen v-if="location === 'splash'"/>
     </v-content>
   </v-app>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue';
+import SplashScreen from './components/SplashScreen.vue';
 
 export default {
   name: 'App',
   components: {
     HelloWorld,
+    SplashScreen,
   },
   data: () => ({
-    //
+    location: 'splash',
   }),
 };
 </script>
---
+++ vyzby-app/src/components/SplashScreen.vue
@@ -0,0 +1,57 @@
+<template>
+  <div id="splash-container">
+    <div class="title-bar">
+      <h1>{{ title }}</h1>
+      <h2>{{ tagline}}</h2>
+    </div>
+    <div class="btn-wrapper">
+      <v-btn outlined color="white">{{ buttonText }}</v-btn>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    title: 'VYZBY',
+    tagline: 'A visceral interactive digital canvas to play and create in.',
+    buttonText: 'Go Play',
+  }),
+};
+</script>
+
+<style lang="scss" scoped>
+#splash-container {
+  height: 100%;
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  overflow: hidden;
+  background-color: #000;
+}
+
+.title-bar {
+  margin-top: 20%;
+  color: $color-primary-blue;
+  text-align: center;
+}
+
+h1 {
+  font-family: 'Montserrat', 'sans-serif';
+  font-weight: 900;
+  font-size: 6.5rem;
+  line-height: 1.5em;
+}
+
+h2 {
+  font-family: 'Roboto', 'sans-serif';
+  font-weight: 300;
+  font-size: 1.5rem;
+  line-height: 1.15em;
+}
+
+.btn-wrapper {
+  margin-top: 20%;
+  text-align: center;
+}
+</style>
