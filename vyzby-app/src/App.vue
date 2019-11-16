6b344cfa775c7b8ca854fa48dc0b7b5b8665a5ea
Jose Conchello <joseconchello@Joses-MacBook-Pro.local>
2019-11-16 16:36:07 -0500

    vyzby-app import p5 function for splash page

--- vyzby-app/src/App.vue
+++ vyzby-app/src/App.vue
@@ -1,29 +1,20 @@
-<template>
-  <v-app>
-    <v-content>
-      <HelloWorld v-if="location ==='main'" />
-      <SplashScreen v-if="location === 'splash'" />
-    </v-content>
-  </v-app>
+<template lang="pug">
+  v-app
+    v-content
+      SplashScreen
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue';
-import SplashScreen from './components/SplashScreen.vue';
+import SplashScreen from '@/components/SplashScreen.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld,
     SplashScreen,
   },
   data: () => ({
-    location: 'splash',
   }),
   events: {
-    'loc-change': function (toLocation) {
-      this.location = toLocation;
-    },
   },
 };
 </script>
--- vyzby-app/src/components/SplashScreen.vue
+++ vyzby-app/src/components/SplashScreen.vue
@@ -1,35 +1,26 @@
-<template>
-  <div id="splash-container">
-    <div id="splash-sketch-background"></div>
-    <div class="content">
-      <div class="title-bar">
-        <h1>{{ title }}</h1>
-        <h2>{{ tagline}}</h2>
-      </div>
-      <div class="btn-wrapper">
-        <v-btn @click.stop="changeLocation()" dark x-large color="#0e83cd">{{ buttonText }}</v-btn>
-        <i @click.stop="dialog = true" id="about" class="material-icons-outlined">info</i>
-      </div>
-    </div>
+<template lang="pug">
+  #splash-container
+    #splash-sketch-background
+    .content
+      .title-bar
+        h1 {{ title }}
+        h2 {{ tagline }}
+      .btn-wrapper
+        v-btn( dark x-large color="#0e83cd") {{ buttonText }}
+        i#about( @click.stop="dialog = true" class="material-icons-outlined") info
 
-    <v-dialog v-model="dialog" max-width="450">
-      <v-card dark color="#000" elevation="10">
-        <v-card-title class="headline">{{ dialogHeadline }}</v-card-title>
-        <v-card-text>
-          <p>{{ dialogTagline }}</p>
-          <ul>
-            <li v-for="(point, index) in dialogPoints" :key="index">{{ point }}</li>
-          </ul>
-        </v-card-text>
-      </v-card>
-    </v-dialog>
-  </div>
+    v-dialog( v-model="dialog" max-width="450")
+      v-card( dark color="#000" elevation="10")
+        v-card-title.headline {{ dialogHeadline }}
+        v-card-text
+          p {{ dialogTagline }}
+          ul
+            li( v-for="(point, index) in dialogPoints" :key="index") {{ point }}
 </template>
 
 <script>
-function changeLocation() {
-  this.$emit('loc-change', 'main');
-}
+// import Particle from '@/js/ParticleMouseClass';
+import ParticleScript from '@/js/ParticleMouseClass';
 
 export default {
   data: () => ({
@@ -50,70 +41,10 @@ export default {
       'Use your body as a brush via the webcam.  ** Only available on some layers',
     ],
   }),
-  methods: {
-    changeLocation,
-  },
+  methods: {},
   mounted() {
-    class Ball {
-      constructor(p5) {
-        this.Xv = p5.random(0, p5.windowWidth);
-        this.Yv = p5.random(0, p5.windowHeight);
-        this.pX = p5.random(0, p5.windowWidth);
-        this.pY = p5.random(0, p5.windowHeight);
-
-        this.X = p5.random(0, p5.windowWidth);
-        this.Y = p5.random(0, p5.windowHeight);
-        this.w = p5.random(1 / thold, thold);
-
-        this.render = function() {
-          if (!p5.mouseIsPressed) {
-            this.Xv /= spifac;
-            this.Yv /= spifac;
-          }
-          this.Xv += drag * (mX - this.X) * this.w;
-          this.Yv += drag * (mY - this.Y) * this.w;
-          this.X += this.Xv;
-          this.Y += this.Yv;
-          p5.line(p5.noise(p5.frameCount * 0.001 ) * 100 + this.X, p5.noise(p5.frameCount) * 20 + this.Y, this.pX, this.pY);
-          this.pX = this.X;
-          this.pY = this.Y;
-        };
-      }
-    }
-
-    let thold = 5;
-    let spifac = 1.05;
-    let drag = 0.001;
-    let big = 250;
-    let bodies = [];
-    let mX = 0;
-    let mY = 0;
-
-    const script = function(p5) {
-      p5.setup = _ => {
-        p5.createCanvas(p5.windowWidth, p5.windowHeight);
-        p5.strokeWeight(1);
-        p5.fill(255, 255, 255);
-        p5.stroke(255, 255, 255, 100);
-        p5.background(0, 0, 0);
-        p5.smooth();
-        for (let i = 0; i < big; i++) {
-          bodies[i] = new Ball(p5);
-        }
-      };
-
-      p5.draw = _ => {
-        p5.background(0, 0, 0, 10);
-        mX += 0.3 * (p5.mouseX - mX);
-        mY += 0.3 * (p5.mouseY - mY);
-
-        for (let i = 0; i < big; i++) {
-          bodies[i].render();
-        }
-      };
-    };
     const P5 = require('p5');
-    new P5(script, 'splash-sketch-background');
+    new P5(ParticleScript, 'splash-sketch-background');
   },
 };
 </script>
