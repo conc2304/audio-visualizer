52029a3d34e0957dabe856bbeb9a2430c20ef5e2
Jose Conchello <joseconchello@Joses-MacBook-Pro.local>
2019-11-16 23:11:45 -0500

    vyzby app - tweak styles

---
+++ vyzby-app/src/components/IconWithTooltip.vue
@@ -0,0 +1,43 @@
+<template lang="pug">
+  .control-icon-wrapper()
+    v-tooltip(
+      right
+    )
+      template( v-slot:activator= "{ on }")
+        v-icon.menu-icon(
+          :id='menuItemData.id'
+          v-on="on"
+          class="global-control helper"
+        ) {{ menuItemData.mdIconText }}
+      span  {{ menuItemData.title }}
+</template>
+
+<script>
+export default {
+  name: 'IconWithTooltip',
+  props: {
+    menuItemData: {
+      type: Object,
+    },
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+i.menu-icon {
+  color: $color-primary-blue;
+  cursor: pointer;
+}
+
+i.menu-icon.inactive {
+  color: $color-inactive-red-hover;
+}
+
+i.menu-icon.inactive:hover {
+  color: $color-inactive-red
+}
+
+i.menu-icon:hover {
+  color: $color-secondary-blue;
+}
+</style>
---
+++ vyzby-app/src/components/LayerControlProperties.vue
@@ -0,0 +1,39 @@
+<template lang="pug">
+  #layer-control-inner
+    h2 Layer Controls
+    v-expansion-panels( accordion multiple focusable)
+      v-expansion-panel(
+        v-for="(item,i) in 5"
+        :key="i"
+      )
+        v-expansion-panel-header.layer-name Layer {{ i }}
+          template( v-slot:actions)
+            v-icon expand_more
+        v-expansion-panel-content banana
+</template>
+
+<script>
+export default {
+  name: 'LayerControlProperties',
+  data: () => ({}),
+};
+</script>
+
+<style lang="scss" scoped>
+#layer-control-inner {
+  width: 100%;
+
+  .v-expansion-panel {
+    background-color: transparent;
+  }
+}
+
+.layer-name {
+  font-size: 18px;
+  font-weight: bold;
+}
+
+h2 {
+  text-align: center;
+}
+</style>
