3b0ce3eab16ce50125d012e919ba9165716b1f7d
Jose Conchello <joseconchello@Joses-MacBook-Pro.local>
2019-11-16 20:42:30 -0500

    vyzby-app theme the menu a little

--- vyzby-app/src/components/SettingsMenu.vue
+++ vyzby-app/src/components/SettingsMenu.vue
@@ -5,6 +5,7 @@
   v-card#settings-menu(
     class="mx-auto"
     width="330"
+    dark
   )
     v-navigation-drawer(
       permanent
@@ -38,25 +39,23 @@ import CompositionControls from '@/components/CompositionControls.vue';
 
 export default {
   components: {
-    CompositionControls
+    CompositionControls,
   },
-  data: () => ({
-  }),
-
-}
+  data: () => ({}),
+};
 </script>
 
 <style lang="scss" scoped>
 #settings-menu::-webkit-scrollbar-track {
   -webkit-box-shadow: inset 0 0 6px rgba(0, 0, 0, 0.1);
-  background-color: #F5F5F5;
-  background-color: #F5F5F5;
+  background-color: #f5f5f5;
+  background-color: #f5f5f5;
   border-radius: 10px;
 }
 
 #settings-menu::-webkit-scrollbar {
   width: 10px;
-  background-color: #F5F5F5;
+  background-color: #f5f5f5;
 }
 
 @media (min-width: 320px) and (max-width: 480px) {
@@ -72,12 +71,14 @@ export default {
 #settings-menu::-webkit-scrollbar-thumb {
   border-radius: 10px;
   background-color: #000000bd;
-  background-image: -webkit-gradient(linear,
-  40% 0%,
-  75% 84%,
-  from(#000000bd),
-  to(#0c6b98),
-  color-stop(.9, #0e83cd));
+  background-image: -webkit-gradient(
+    linear,
+    40% 0%,
+    75% 84%,
+    from(#000000bd),
+    to(#0c6b98),
+    color-stop(0.9, #0e83cd)
+  );
 }
 
 #settings-menu::-webkit-scrollbar-track {
@@ -91,6 +92,12 @@ export default {
   top: 0;
   bottom: 0;
   left: 0;
+  background-color: #000000bd;
+  overflow-y: scroll;
+  border-right: 1px solid #292929;
+}
+
+#layer-control-menu {
   background-color: #000000bd;
   overflow-y: scroll;
   border-right: 1px solid #292929;
