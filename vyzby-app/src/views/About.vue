9e4772293a213ddd4056e20bd0feb5981696644d
clyzby <jose.conchello@gmail.com>
2019-11-20 13:54:15 -0500

    vyzby-app use axios for api things

--- vyzby-app/src/views/About.vue
+++ vyzby-app/src/views/About.vue
@@ -1,5 +1,28 @@
-<template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+<template lang="pug">
+  .about
+    h1( v-if="title") {{ title }}
+    h3( v-if="subtitle") {{ subtitle }}
 </template>
+
+<script>
+const axios = require('axios');
+
+export default {
+  data: () => ({
+    title: '',
+    subtitle: '',
+  }),
+
+  mounted() {
+    this.title = axios.get(
+      'https://my-json-server.typicode.com/conc2304/test-api/db',
+    )
+    .then(response => {
+      console.log(response);
+      let data = response.data.about;
+      this.title = data.title;
+      this.subtitle = data.subtitle;
+    });
+  },
+};
+</script>
