7f7b4781c22aaf33d3a30d984ce8324020c5fad0
clyzby <jose.conchello@gmail.com>
2019-11-20 12:15:44 -0500

    vyzby-app add third sketch

--- vyzby-app/src/js/services/SketchRegistration.js
+++ vyzby-app/src/js/services/SketchRegistration.js
@@ -1,9 +1,11 @@
 import CenterWave from '@/js/sketches/SketchConstructorCenterWave';
 import WEBGLWave from '@/js/sketches/SketchConstructorWEBGLWave';
+import OuterWaves from '../sketches/SketchConstructorOuterWaves';
 
 const registeredSketches = [];
 
 registeredSketches.push(new CenterWave(window.innerWidth, window.innerHeight));
+registeredSketches.push(new OuterWaves(window.innerWidth, window.innerHeight));
 registeredSketches.push(new WEBGLWave(window.innerWidth, window.innerHeight));
 
 export default registeredSketches;
