b3063b49e854e3dd15996ff22c20c0bc6a90e9ff
Jose Conchello <joseconchello@Joses-MacBook-Pro.local>
2019-11-24 20:22:09 -0500

    vyzby-app  no ui slider working ish

---
+++ vyzby-app/src/components/SketchParameterControlNumeric.vue
@@ -0,0 +1,28 @@
+<template >
+  <div class='sketch-parameter-control'>
+
+    <span>{{ parameter.displayLabel }}</span>
+
+    <v-nus
+      v-if="parameter.attrType === 'numeric'"
+      :config="{ connect: true, connectColors: ['#b2b2b2', 'red', '#0e83cd', '#b2b2b2'], range: { min: [0], max: [100], }, tooltips: true,}"
+      :value="[this.parameter.min, this.parameter.currentValue, this.parameter.max]"
+      @update="values = $event"
+     />
+  </div>
+
+</template>
+
+<script>
+export default {
+  data: () => ({}),
+
+  props: {
+    sketchSelected: {},
+    parameter: {
+      type: Object,
+    },
+  },
+
+};
+</script>
