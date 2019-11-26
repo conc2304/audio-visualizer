d5891bb3d19015ea2b925f27450af9f02cf27f8f
clyzby <jose.conchello@gmail.com>
2019-11-26 15:25:58 -0500

    vyzby- app add audio reactive control component

---
+++ vyzby-app/src/components/SketchParameterControlAudio.vue
@@ -0,0 +1,44 @@
+<template lang="pug">
+  .audio-parameter-wrapper
+
+    KnobControl(
+      v-if="parameter.audio && gain"
+      :min="0"
+      :max="100"
+      :size="40"
+      primary-color="#0eb1ff"
+      secondary-color="#0e83cd"
+      text-color="#FFFFFF"
+      v-model="gain"
+    )
+    p Gain
+</template>
+
+<script>
+import KnobControl from 'vue-knob-control'
+
+export default {
+  data: () => ({
+    gain: false
+  }),
+
+  components: {
+    KnobControl,
+  },
+
+  props: {
+    parameter: {
+      type: Object,
+    }
+  },
+
+  mounted() {
+    let gain = this.parameter.audio.gain;
+
+    this.gain = gain * 100;
+    console.log(this.parameter.audio.gain);
+    // console.log(this.parameter.audio.gain);
+    // this.gain = this.parameter.audio.gain;
+  }
+}
+</script>
