5f5128d89395a278468df9734258cbb1d59a6206
Jose Conchello <joseconchello@Joses-MacBook-Pro.local>
2019-12-12 21:16:02 -0500

    vyzby-app fix snackbar

---
+++ src/components/PresetAssignSnackbar.vue
@@ -0,0 +1,68 @@
+<template lang="pug">
+  v-snackbar(
+      v-model="snackbarOpen"
+      left
+      multi-line
+      :timeout=0
+      vertical
+      dark
+    )
+      p( class="text-center") Select a slot to save the current compoistion settings to a slot
+      small( class="text-center") Selecting a full preset slot will override the slot
+      br
+      .preset-selector-wrapper
+        v-list-item(
+          @click="saveToPreset(i)"
+          v-for="(preset, i) in presetSlots"
+          :key="`preset-selector${i}`"
+          :class=" preset.empty ? 'preset-empty' : 'preset-full'"
+        )
+          v-icon(
+
+          ) {{ preset.iconText }}
+        v-list-item(
+          @click="closeSnackBar()"
+          dark
+        )
+          v-icon() close
+</template>
+
+<script>
+export default {
+  props: {
+    presetSlots: {
+      type: Array,
+      required: true,
+    },
+    snackbarOpen: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+
+  data: () => ({
+  }),
+
+  methods: {
+    saveToPreset(i) {
+      this.presetSlots[i].empty = false;
+      this.$emit('update_snackbar', false);
+    },
+
+    closeSnackBar() {
+      this.$emit('update_snackbar', false);
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.preset-full i {
+  color: $color-primary-blue;
+}
+
+.preset-empty i {
+  color: $color-off-white;
+}
+</style>
