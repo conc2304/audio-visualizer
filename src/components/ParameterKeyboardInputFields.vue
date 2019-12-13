c668a88cb8c5a1047680d7ced7b64ad32e81ff6e
Jose Conchello <joseconchello@Joses-MacBook-Pro.local>
2019-12-13 16:10:14 -0500

    vyzby-app assign keys only after validation

--- src/components/ParameterKeyboardInputFields.vue
+++ src/components/ParameterKeyboardInputFields.vue
@@ -11,7 +11,6 @@
         clear-icon="close"
         color="blue"
         maxlength="1"
-        @change="setKey()"
         @click:clear="clear('key')"
         :rules="[val => { return (testKeyStroke(val)) }]"
         outlined
@@ -28,7 +27,6 @@
         clearable
         clear-icon="close"
         color="blue"
-        @change="setKey()"
         @click:clear="clear('value')"
         :rules="[val => { return (testValueMinMax(val, parameter)) }]"
         outlined
@@ -42,6 +40,7 @@ import KeyboardControlsService from '@/js/services/KeyboardControlsService';
 
 export default {
   data: () => ({
+    valid: true,
     keyboardKey: null,
     parameterValue: null,
   }),
@@ -74,7 +73,6 @@ export default {
     },
 
     testKeyStroke(value) {
-
       // alphabet charcodes fo A-Z = [65 - 90]
       // alphabet charcodes fo a-z = [97 - 122]
       // number 0-9 = [49 - 57]
@@ -85,31 +83,36 @@ export default {
       this.keyboardKey = value.toUpperCase();
       const keyVal = value.toUpperCase().charCodeAt(0);
       const validKey =
-        between(keyVal, 65, 90) || between(keyVal, 49, 57) || between(keyVal, 97, 122);
-      return validKey || 'Key must be either A-Z, or 0-9';
+        this.isBetween(keyVal, 65, 90) ||
+        this.isBetween(keyVal, 49, 57) ||
+        this.isBetween(keyVal, 97, 122);
 
-      function between(x, min, max) {
-        return x >= min && x <= max;
+      if (validKey) {
+        this.setKey();
+        return true;
+      } else {
+        return 'Key must be either A-Z, or 0-9';
       }
     },
 
     testValueMinMax(value, parameter) {
-      let valid = true;
-      let msg = '';
-
-      if (!parameter) {
+      if (!value || !parameter) {
         return true;
       }
-
-      if (value <= parameter.min) {
-        valid = false;
-        msg = `Value must be more than ${parameter.min}`;
-      } else if (value >= parameter.max) {
-        valid = false;
-        msg = `Value must be less than ${parameter.max}`;
+      if (value <= parseFloat(parameter.min)) {
+        return `Value must be more than ${parameter.min}`;
+      }
+      if (value >= parseFloat(parameter.max)) {
+        return `Value must be less than ${parameter.max}`;
       }
 
-      return valid || msg;
+      this.setKey();
+
+      return true;
+    },
+
+    isBetween(x, min, max) {
+      return x >= min && x <= max;
     },
 
     hint() {
@@ -123,6 +126,4 @@ export default {
 };
 </script>
 
-<style lang="scss">
-
-</style>
+<style lang="scss"></style>
