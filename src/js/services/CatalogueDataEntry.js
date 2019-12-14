df1c572fb0562e3b36071cf603d7f167023c5b62
Jose Conchello <joseconchello@Joses-MacBook-Pro.local>
2019-12-14 15:33:51 -0500

    vyzby-app add catalogue search

--- src/js/services/CatalogueDataEntry.js
+++ src/js/services/CatalogueDataEntry.js
@@ -4,16 +4,16 @@ class CatalogueInfoProperty {
    * @param {Constructor} constructor
    * @param {String} title
    * @param {String} description
-   * @param {Array} categories     // a list of categories to desribe the sketch to filter on
+   * @param {Array} tags     // a list of categories to desribe the sketch to filter on
    * @param {String} creator
    * @param {String} avatarURI
    * @param {Number} cpuUsage      // arbitrarily weighted value of how taxing this sketch is.  scale is 1 to 10  where 10 means it should run by itself.
    */
-  constructor(constructor, title, description, categories, creator, avatarURI, cpuUsage) {
+  constructor(constructor, title, description, tags, creator, avatarURI, cpuUsage) {
     this.classConstructor = constructor;
     this.title = title;
     this.description = description;
-    this.filterCategories = categories;
+    this.tags = tags;
     this.creator = creator;
 
     this.gifURI = avatarURI;
