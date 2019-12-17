9e1b1e04f4e0ea043b290a6bd87c29a6de449c24
clyzby <jose.conchello@gmail.com>
2019-12-17 15:55:10 -0500

    vyzby-app add login vuelidation

--- src/components/LoginPane.vue
+++ src/components/LoginPane.vue
@@ -1,205 +1,45 @@
 <template lang="pug">
   v-container#login-container
-    v-form( @submit.prevent="submit")
-      .form-group(:class="{ 'form-group--error': $v.username.$error }")
-        .login-pane( v-if="loginPane === 'userLogin'")
-          v-text-field(
-            label="Userame"
-            required
-            :rules="usernameRules"
-            v-model.trim="$v.username.$model"
-          )
+    LoginUserPane(
+      v-if="loginPane === 'userLogin'"
+      @update_login_pane="updateLoginPane"
+    )
+    LoginRegistrationPane(
+      v-if="loginPane === 'registration'"
+      @update_login_pane="updateLoginPane"
+    )
 
-          v-text-field(
-            type="password"
-            label="Password"
-            required
-            validate-on-blur
-            :rules="passwordRules"
-            v-model="password"
-          )
-
-          h5.text-center Don't Have an Account?
-          h6#account-creation(
-            @click="loginPane = 'createAccount'"
-            class="text-center"
-          ) create account
-
-        #account-creation-pane( v-if="loginPane === 'createAccount'")
-          v-row
-            v-col(md="6")
-              v-text-field(
-                label="First Name"
-                required
-                :rules="nameRules"
-                v-model.trim="firstName"
-              )
-            v-col(md="6")
-              v-text-field(
-                label="Last Name"
-                required
-                :rules="nameRules"
-                v-model.trim="lastName"
-              )
-          v-row
-            v-col(md="12")
-              v-text-field(
-                label="Email"
-                type="email"
-                required
-                :rules="emailRules"
-                v-model.trim="email"
-              )
-          v-row
-            v-col(md="12")
-              v-text-field(
-                label="Userame"
-                required
-                :rules="nameRules"
-                v-model.trim="username"
-              )
-          v-row
-            v-col(md="12")
-              v-text-field(
-                type="password"
-                label="Password"
-                :rules="passwordRules"
-                validate-on-blur
-                required
-                v-model.trim="password"
-              )
-          v-row
-            v-col(md="12")
-              v-text-field(
-                type="password"
-                label="Confirm Password"
-
-                required
-                v-model.trim="passwordConfirm"
-              )
-
-              h5.text-center Already have an account?
-              h6#account-creation(
-                @click="loginPane = 'userLogin'"
-                class="text-center"
-              ) login
-
-      .submission-wrapper
-        v-btn(
-          type="submit"
-          :disabled="submitStatus === 'PENDING'"
-          dark large color="#0e83cd" outlined
-        ) Submit
-
-        p.submit-status(v-if="submitStatus === 'OK'") Thanks for your submission!
-        p.submit-status(v-if="submitStatus === 'ERROR'") Please fill the form correctly.
-        p.submit-status(v-if="submitStatus === 'PENDING'") Sending...
 </template>
 
 <script>
-import { required, minLength } from 'vuelidate/lib/validators';
+import LoginUserPane from '@/components/LoginUserPane.vue';
+import LoginRegistrationPane from '@/components/LoginRegistrationPane.vue';
 
 export default {
-  data() {
-    return {
-      loginPane: 'userLogin',
-      password: '',
-      passwordConfirm: '',
-      username: '',
-      firstName: '',
-      lastName: '',
-      email: '',
-      usernameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length >= 6) || 'Name must be at least 6 characters',
-      ],
+  data: () => ({
+    loginPane: 'userLogin',
+  }),
 
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => (v && v.length >= 3) || 'Name must be at least 3 characters',
-      ],
-      passwordRules: [
-        v => !!v || 'Password is required',
-        v => v === 'VYZBY' || 'Password must be "VYZBY"',
-      ],
-      passwordConfirmRules: [
-        v => !!v || 'Password confirmation required',
-        v => v === this.password || 'Passwords do not match',
-      ],
-
-      emailRules: [
-        v =>
-          v.match(
-            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-          ) || 'Invalid Email address',
-      ],
-
-      submitStatus: null,
-    };
+  components: {
+    LoginUserPane,
+    LoginRegistrationPane,
   },
 
-  props: {
-    isLoggedIn: {
-      type: Boolean,
-      default: false,
-    },
-  },
-
-  validations: {
-    username: {
-      required,
-      minLength: minLength(6),
-    },
-  },
   methods: {
-    submit() {
-      this.$v.$touch();
-      if (this.$v.$invalid) {
-        this.submitStatus = 'ERROR';
-      } else {
-        this.submitStatus = 'PENDING';
-        setTimeout(() => {
-          this.submitStatus = 'OK';
-          const userObj = {
-            loggedIn: true,
-            username: this.username,
-          };
-          localStorage.setItem('user', JSON.stringify(userObj));
-
-          this.$emit('user_login_event', userObj);
-
-          this.$v.$reset();
-          (this.submitStatus = null), (this.username = '');
-        }, 500);
-      }
+    updateLoginPane(paneLocation) {
+      this.loginPane = paneLocation;
     },
   },
 };
 </script>
 
-<style lang="scss" scoped>
-#account-creation:hover {
-  text-decoration: underline;
-}
-.error {
-  text-align: center;
-}
-
-.submission-wrapper {
-  margin: 20px auto 0;
-  text-align: center;
-}
-
-.submit-status {
-  margin-top: 10px;
-}
-</style>
-
 <style lang="scss">
 /* Change Autocomplete styles in Chrome*/
 
 #login-container {
-  input:-internal-autofill-selected,
+
+  padding: 2rem 4rem;
+
   input:-webkit-autofill,
   input:-webkit-autofill:hover,
   input:-webkit-autofill:focus,
@@ -208,10 +48,30 @@ export default {
   textarea:-webkit-autofill:focus,
   select:-webkit-autofill,
   select:-webkit-autofill:hover,
+  input:-internal-autofill-selected,
   select:-webkit-autofill:focus {
     background-color: transparent;
     -webkit-box-shadow: 0 0 0px 1000px #000 inset;
     -webkit-text-fill-color: #fff;
+  }
+
+  .account-creation {
+      margin: 0 auto;
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+  .error {
+    text-align: center;
+  }
+
+  .submission-wrapper {
+    margin: 2rem auto 0;
+    text-align: center;
+  }
+
+  .submit-status {
+    margin-top: 10px;
   }
 }
 </style>
--- src/store/index.js
+++ src/store/index.js
@@ -6,20 +6,22 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    user: {},
     sketchIndexSelected: -1,
     RegisteredSketches,
     updatedTimestamp: 0,
   },
   mutations: {
-    updateSketchIndexSelected(state, payload){
+    updateSketchIndexSelected(state, payload) {
       state.sketchIndexSelected = payload;
     },
     updateRegisteredSketches(state, payload) {
       state.RegisteredSketches = payload;
-    }
+    },
+    updateUserLoggedIn(state, payload) {
+      state.userLoggedIn = payload;
+    },
   },
-  actions: {
-  },
-  modules: {
-  },
+  actions: {},
+  modules: {},
 });
