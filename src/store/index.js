8d344ff44adcbd61af3ddf9147e5a6c50a1d1173
clyzby <jose.conchello@gmail.com>
2019-12-16 19:20:54 -0500

    vyzby-app start on state management

--- src/store/index.js
+++ src/store/index.js
@@ -1,12 +1,21 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
+import RegisteredSketches from '@/js/services/SketchRegistration';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
+    sketchIndexSelected: -1,
+    RegisteredSketches,
   },
   mutations: {
+    updateSketchIndexSelected(state, payload){
+      state.sketchIndexSelected = payload;
+    },
+    updateRegisteredSketches(state, payload) {
+      state.RegisteredSketches = payload;
+    }
   },
   actions: {
   },
