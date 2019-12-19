d71d8097bf7b1b422cdc320046d6271fd682e70a
clyzby <jose.conchello@gmail.com>
2019-12-19 10:55:01 -0600

    vyzby-app  finalize app settings menu and add a login pane

--- src/components/AppSettingsMenu.vue
+++ src/components/AppSettingsMenu.vue
@@ -1,5 +1,12 @@
 <template lang="pug">
   #app-settings-menu
+
+    v-dialog( v-model="loginDialog" max-width="600")
+      v-card(  color="#000" elevation="10")
+        v-card-title.headline Login
+        v-card-text
+          LoginPane()
+
     v-tooltip( left)
       template( v-slot:activator= "{ on }")
         v-icon.settings-btn(
@@ -12,40 +19,74 @@
       v-show="settingsOpen"
       dense dark
     )
-      v-list-item(
+      v-tooltip(
         v-for="(item, i) in preferences"
-        @click="clickHandler(item.action)"
+        :disabled="!userIsLoggedIn && item.title === 'Home'"
+        :key="i"
+        left
+      )
+        template( v-slot:activator= "{ on }")
+          .hover-wrapper(
+            v-on="on"
+          )
+            v-list-item(
+              @click="clickHandler(item.action)"
+              :disabled="!userIsLoggedIn && item.title !== 'Home'"
+            )
+              v-list-item-icon
+                v-icon(
+                  :class="{ 'disabled': !userIsLoggedIn && item.title !== 'Home' }"
+                ) {{ item.icon }}
+              v-list-item-content
+                v-list-item-title( v-text="item.title")
+        span {{ item.tooltipText }}
+      v-list-item(
+        v-show="!userIsLoggedIn"
+        @click="loginDialog = true"
       )
         v-list-item-icon
-          v-icon( v-text="item.icon")
+          v-icon account_box
         v-list-item-content
-          v-list-item-title( v-text="item.title")
+          v-list-item-title Login
 
 
 </template>
 
 <script>
+import LoginPane from '@/components/LoginPane.vue';
+
 export default {
+  components: {
+    LoginPane
+  },
+
   data: () => ({
     settingsOpen: false,
+    loginDialog: false,
+
     preferences: [
       {
         title: 'Home',
         action: 'goToHome',
         icon: 'home',
+        tooltipText: ''
       },
       {
         title: 'Save Session',
         action: 'saveSession',
         icon: 'save',
+        tooltipText: 'Login to save your session'
       },
       {
         title: 'Open Session',
         action: 'openSession',
         icon: 'folder_open',
+        tooltipText: 'Login to open a saved session'
       },
     ],
   }),
+
+
 
   methods: {
     clickHandler(functionName) {
@@ -60,6 +101,14 @@ export default {
       this.$router.push('/');
     },
   },
+
+  computed: {
+    userIsLoggedIn() {
+      const userObj = JSON.parse(localStorage.getItem('user'));
+      return (userObj != null && userObj.loggedIn);
+    },
+  },
+  mounted() {},
 };
 </script>
 
@@ -83,6 +132,10 @@ export default {
 
   .preferences-menu {
     clear: both;
+  }
+
+  i.disabled {
+    color: $color-off-white;
   }
 }
 </style>
