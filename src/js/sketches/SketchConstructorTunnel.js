cd56da4f093e398b005d49971543f6e6fc0a5867
clyzby <jose.conchello@gmail.com>
2020-02-04 14:16:14 -0500

    vyzby-app test some features

--- src/js/sketches/SketchConstructorTunnel.js
+++ src/js/sketches/SketchConstructorTunnel.js
@@ -9,7 +9,7 @@ import SketchCatalogue from '@/js/services/SketchCatalogue';
 
 class Tunnel {
   constructor(windowWidth, windowHeight, p5) {
-    this.sid =  Utils.guidGenerator();
+    this.sid = Utils.guidGenerator();
 
     this.catalogueInfo = new CatalogueDataEntry(
       this.constructor,
@@ -31,7 +31,7 @@ class Tunnel {
     this.radius = new NumericProperty('Size', 'Base', 20, 0, 2000, 0.7);
 
     this.iterations = new NumericProperty('Number of Objects', 'Base', 3, 0, 40, 0.5);
-    this.spacing = new NumericProperty('Z Spacing', 'Base', 3, 20, 200, 0.5);
+    this.spacing = new NumericProperty('Z Spacing', 'Base', 3, -100, 500, 0.5);
 
     this.shape = new VariableProperty('Shape', 'Base', 'square', [
       'line',
@@ -47,7 +47,6 @@ class Tunnel {
 
     this.hue = new NumericProperty('Color', 'Color', 200, 0, 360, 0.1);
     this.saturation = new NumericProperty('Saturation', 'Color', 100, 0, 100, 0.1);
-
   }
 }
 
@@ -57,28 +56,46 @@ Tunnel.prototype.render = function(p5) {
   let xPos = 0;
   let yPos = 0;
 
+  p5.strokeWeight(2);
+  // p5.stroke(this.hue.currentValue, this.saturation.currentValue, 100);
+  // p5.fill(360, 180, 10);
 
-  let i = 0
+  p5.noFill();
+
+  let i = 0;
   while (i < this.iterations.currentValue) {
+    p5.push();
+
+    const depth = i * this.spacing.currentValue;
+    const movement = -p5.sin(p5.frameCount * 0.001) * 500;
+    const radius = this.radius.currentValue;
+    const test = p5.map(p5.sin(p5.frameCount * 0.005 + i), 0, 1, 0, 360);
 
     p5.translate(
       this.translateX.currentValue,
       this.translateY.currentValue,
-      this.translateZ.currentValue - (i * this.spacing.currentValue),
+      this.translateZ.currentValue + 200 - i * this.spacing.currentValue + movement,
     );
 
 
-    p5.strokeWeight(1 + (i * 0.5));
-    p5.stroke(this.hue.currentValue, this.saturation.currentValue, 100);
-    p5.noFill();
+    if (i === 0) {
+      p5.stroke(360, this.saturation.currentValue, 100);
+    } else {
+      const calc = p5.sin(p5.noise((p5.frameCount + ( 2.5 * i) ) * 0.1 ));
+      const wave = p5.map(calc, 0, 1, 0, 100);
+      const hue = p5.map(calc, 0, 1, Number(this.hue.min), Number(this.hue.max));
+      console.log(hue);
+      p5.stroke(hue, this.saturation.currentValue, wave);
+    }
 
+    this.renderShape(p5, xPos, yPos, radius);
+    p5.pop();
 
-    this.renderShape(p5, xPos, yPos, this.radius.currentValue);
     i++;
   }
 
   p5.pop();
-}
+};
 
 Tunnel.prototype.rotateShape = helper.rotateShape;
 Tunnel.prototype.setColor = helper.setColor;
