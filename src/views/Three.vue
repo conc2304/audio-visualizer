293af118eb88f8ecc28ec8174136382d1ae8c675
clyzby <jose.conchello@gmail.com>
2020-04-07 17:16:17 -0400

    threejs-tut save name change

---
+++ src/views/Three.vue
@@ -0,0 +1,30 @@
+<template lang="pug">
+
+  .three-tut-wrapper
+    nav
+      ul
+        li( v-for='route in routes' :key='route.name')
+          router-link(:to="route.path" exact) {{ route.name }}
+    router-view
+
+    h1 Three test
+
+</template>
+
+<script>
+// const axios = require('axios');
+import * as THREE from 'three';
+import Router from '@/router/index.js';
+
+export default {
+  data: () => ({
+    title: '',
+    subtitle: '',
+    routes: Router.options.routes,
+  }),
+
+  mounted() {
+    console.log(Router);
+  },
+};
+</script>
