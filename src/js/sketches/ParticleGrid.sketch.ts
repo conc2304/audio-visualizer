baa4ea8b976578b2df877f4125ee7d7a0c13d3eb
Jose Conchello <joseconchello@Joses-MacBook-Pro.local>
2021-07-24 09:32:16 -0500

    grid - add mirroring

--- src/js/sketches/ParticleGrid.sketch.ts
+++ src/js/sketches/ParticleGrid.sketch.ts
@@ -35,17 +35,24 @@ export default class ParticleGrid implements P5Sketch {
   public noiseZoom = new NumericProperty('Noise Zoom', 'Base', -250, -700, 250, 0.7);
   public columns = new NumericProperty('Columns', 'Base', 20, 5, 40, 0.7);
   public rows = new NumericProperty('Rows', 'Base', 20, 5, 40, 0.7);
-  public noiseVelocity = new NumericProperty('Noise Speed', 'Base', 0, 1, 100, 0.7);
+  public noiseVelocity = new NumericProperty('Noise Speed', 'Base', 20, 1, 100, 0.7);
   public noiseAmount = new NumericProperty('Noise Amount', 'Base', 50, 0, 100, 0.7);
   public noiseAmplitude = new NumericProperty('Noise Amplitude', 'Base', 3, 1, 8, 0.7);
+  public mirror = new VariableProperty('Mirror', 'Base', 'None', [
+    'None',
+    'X',
+    'Y',
+    'X & Y'
+  ]);
 
   public strokeHue = new NumericProperty('Line Color', 'Color', 200, 0, 360, 0.7);
   public fillHue = new NumericProperty('Fill Color', 'Color', 200, 0, 360, 0.7);
   public saturation = new NumericProperty('Saturation', 'Color', 100, 0, 100, 0.7);
-  public stroke = new VariableProperty('Outline and Fill', 'Color', 'Outline', [
+  public stroke = new VariableProperty('Outline and Fill', 'Color', 'Fill + Outline', [
     'Outline',
     'Fill + Outline',
   ]);
+
 
 
   private setColor(sketch: p5, noise?: number) {
@@ -78,22 +85,45 @@ export default class ParticleGrid implements P5Sketch {
     const factor = this.noiseAmount.currentValue * 0.0007;
     const velocity = this.noiseVelocity.currentValue * 0.0001;
     const amplitude = this.noiseAmplitude.currentValue;
+    const middleCol = Math.ceil(this.columns.currentValue * 0.5);
+    const middleRow = Math.ceil(this.rows.currentValue * 0.5);
+    const mirrorXOn = this.mirror.currentValue.indexOf("X") > -1;
+    const mirrorYOn = this.mirror.currentValue.indexOf("Y") > -1;
 
     for (let y = 0; y < this.columns.currentValue; y++) {
       for (let x = 0; x < this.rows.currentValue; x++) {
+
         const xPos = horizontalPadding + (hSpacing * y) - this.windowWidth * 0.5;
         const yPos = verticalPadding + (vSpacing * x) - this.windowHeight * 0.5;
-        const n = this.simplex.noise3D(x * factor, y * factor, sketch.frameCount * velocity) * amplitude;
+
+        const noiseX = (mirrorXOn && y > middleRow) ? this.rows.currentValue - y : y;
+        const noiseY = (mirrorYOn && x > middleCol) ? this.columns.currentValue - x : x;
+
+        const n = this.simplex.noise3D(noiseX * factor, noiseY * factor, sketch.frameCount * velocity) * amplitude;
         const i = Math.floor(sketch.map(sketch.cos(n) * sketch.cos(n), -0.5, 0.8, 0, 1000));
+
+        // 1 - 2 - 3 - 4 - 5 - 5 - 4 - 3 - 2 - 1
+        // 1 - 2 - 3 - 4 - 5 - 5 - 4 - 3 - 2 - 1
+        // 1 - 2 - 3 - 4 - 5 - 5 - 4 - 3 - 2 - 1
+
 
         sketch.push();
         this.setColor(sketch, i);
-        console.log(this.noiseZoom.currentValue);
         sketch.translate(xPos, yPos, this.noiseZoom.currentValue);
         sketch.box(hSpacing * 0.95, vSpacing * 0.95, i);
         sketch.pop();
       }
     }
+  }
+
+  renderGridParticleBox(sketch: p5, i: number, xPos: number, yPos: number) {
+
+
+    // sketch.push();
+    // this.setColor(sketch, i);
+    // sketch.translate(xPos, yPos, this.noiseZoom.currentValue);
+    // sketch.box(hSpacing * 0.95, vSpacing * 0.95, i);
+    // sketch.pop();
   }
 
   easeInto = easeInto;
