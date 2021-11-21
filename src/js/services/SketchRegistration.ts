8dad601ce14a51bea7c6f16a45fddcdb04be009c
Jose Conchello <jose.conchello@gmail.com>
2021-11-20 18:17:11 -0600

    master: start a rotary tunnel sketch

--- src/js/services/SketchRegistration.ts
+++ src/js/services/SketchRegistration.ts
@@ -7,16 +7,18 @@ import Parametric from '@/js/sketches/SketchConstructorParametric';
 import Tunnel from '@/js/sketches/SketchConstructorTunnel';
 import BodyBrush from '@/js/sketches/SketchConstructorBodyBrush';
 import ParticleGrid from '../sketches/ParticleGrid.sketch';
+import RotaryTunnel from '../sketches/RotaryTunnel.sketch';
 
 const RegisteredSketches: Array<any> = [];
 
-SketchCatalogue.push(new CenterWave(window.innerWidth, window.innerHeight));
-SketchCatalogue.push(new OuterWaves(window.innerWidth, window.innerHeight));
-SketchCatalogue.push(new WEBGLWave(window.innerWidth, window.innerHeight));
-SketchCatalogue.push(new Parametric(window.innerWidth, window.innerHeight));
-SketchCatalogue.push(new Tunnel(window.innerWidth, window.innerHeight));
-SketchCatalogue.push(new BodyBrush(window.innerWidth, window.innerHeight));
-SketchCatalogue.push(new ParticleGrid(window.innerWidth, window.innerHeight));
+// SketchCatalogue.push(new CenterWave(window.innerWidth, window.innerHeight));
+// SketchCatalogue.push(new OuterWaves(window.innerWidth, window.innerHeight));
+// SketchCatalogue.push(new WEBGLWave(window.innerWidth, window.innerHeight));
+// SketchCatalogue.push(new Parametric(window.innerWidth, window.innerHeight));
+// SketchCatalogue.push(new Tunnel(window.innerWidth, window.innerHeight));
+// SketchCatalogue.push(new BodyBrush(window.innerWidth, window.innerHeight));
+// SketchCatalogue.push(new ParticleGrid(window.innerWidth, window.innerHeight));
+SketchCatalogue.push(new RotaryTunnel(window.innerWidth, window.innerHeight));
 
 
 let i = 0;
