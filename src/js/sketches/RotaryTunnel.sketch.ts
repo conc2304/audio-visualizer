12656b93b22e88a440f10596563a23f7752ec87f
Jose Conchello <jose.conchello@gmail.com>
2021-11-20 23:11:34 -0600

    master: fix fanning

--- src/js/sketches/RotaryTunnel.sketch.ts
+++ src/js/sketches/RotaryTunnel.sketch.ts
@@ -37,15 +37,15 @@ export default class RotaryTunnel implements P5Sketch {
   public pointSize = new NumericProperty('Point Size', 'Base', 40, 15, 400, 0.7, 1);
   public pointsQty = new NumericProperty('Points', 'Base', 8, 1, 40, 0.7, 1);
   public rotationVelocity = new NumericProperty('Rotation Speed', 'Base', 0.25, -2, 2, 0.7, 0.1);
-  public fanAmoutn = new NumericProperty('Fan Amount', 'Base', 360, 0, 360, 0.7, 1);
-  public zDepthDensity = new NumericProperty('Z Depth', 'Base', 5, 1, 20, 0.7, 1)
+  public fanAmount = new NumericProperty('Fan Amount', 'Base', 360, 0, 360, 0.7, 1);
+  public zDepthDensity = new NumericProperty('Z Depth', 'Base', 5, 1, 20, 0.7, 1);
 
   public render(sketch: p5): void {
     const pointSize = this.pointSize.currentValue;
     const pointsQty = Math.floor(this.pointsQty.currentValue);
     const orbitRadius = this.orbitRadius.currentValue;
 
-    const theta = this.fanAmoutn.currentValue / pointsQty;
+    const theta = this.fanAmount.currentValue / pointsQty;
 
     // for (let z = 0; z > this.zMin; z - (z * (this.zMin this.zDepthDensity.currentValue))){
 
@@ -53,7 +53,7 @@ export default class RotaryTunnel implements P5Sketch {
 
     for (let i = 0; i < pointsQty; i++) {
       sketch.push();
-      const x = orbitRadius * Math.cos(theta * i);
+      const x = orbitRadius * Math.sin(theta * i);
       const y = orbitRadius * Math.cos(theta * i);
       sketch.translate(x, y, 0);
       sketch.sphere(pointSize);
