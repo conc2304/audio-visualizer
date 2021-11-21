268fbf6833896358e0fb0e25f76d810494c3121d
Jose Conchello <jose.conchello@gmail.com>
2021-11-20 23:08:43 -0600

    master: save stuff

--- src/js/sketches/RotaryTunnel.sketch.ts
+++ src/js/sketches/RotaryTunnel.sketch.ts
@@ -33,30 +33,35 @@ export default class RotaryTunnel implements P5Sketch {
    * Sketch Parameters
    * */
 
-  public orbitRadius = new NumericProperty('Radius', 'Base', 25, 5, 500, 0.7, 1);
-  public pointSize = new NumericProperty('Point Size', 'Base', 25, 15, 400, 0.7, 1);
-  public pointsQty = new NumericProperty('Points', 'Base', 25, 1, 40, 0.7, 1);
-  public rotationVelocity = new NumericProperty('Rotation Speed', 'Base', 0, -2, 2, 0.7, 0.1);
+  public orbitRadius = new NumericProperty('Radius', 'Base', 80, 5, 500, 0.7, 1);
+  public pointSize = new NumericProperty('Point Size', 'Base', 40, 15, 400, 0.7, 1);
+  public pointsQty = new NumericProperty('Points', 'Base', 8, 1, 40, 0.7, 1);
+  public rotationVelocity = new NumericProperty('Rotation Speed', 'Base', 0.25, -2, 2, 0.7, 0.1);
+  public fanAmoutn = new NumericProperty('Fan Amount', 'Base', 360, 0, 360, 0.7, 1);
+  public zDepthDensity = new NumericProperty('Z Depth', 'Base', 5, 1, 20, 0.7, 1)
 
   public render(sketch: p5): void {
     const pointSize = this.pointSize.currentValue;
     const pointsQty = Math.floor(this.pointsQty.currentValue);
     const orbitRadius = this.orbitRadius.currentValue;
-    // console.log('pointsQty', pointsQty)
 
-    const theta = 360 / pointsQty;
+    const theta = this.fanAmoutn.currentValue / pointsQty;
 
-    for (let i = 1; i < pointsQty; i++) {
+    // for (let z = 0; z > this.zMin; z - (z * (this.zMin this.zDepthDensity.currentValue))){
+
+    // }
+
+    for (let i = 0; i < pointsQty; i++) {
       sketch.push();
-      const x = Math.sin(deltaRotation + i * theta) * orbitRadius;
-      const y = Math.cos(deltaRotation + i * theta) * orbitRadius;
+      const x = orbitRadius * Math.cos(theta * i);
+      const y = orbitRadius * Math.cos(theta * i);
       sketch.translate(x, y, 0);
       sketch.sphere(pointSize);
       sketch.pop();
     }
 
-    // this.updateRotationDelta();
-    deltaRotation += this.rotationVelocity.currentValue * 0.1;
+    this.updateRotationDelta();
+    // deltaRotation += this.rotationVelocity.currentValue * 0.1;
 
     sketch.plane(50, 40);
   }
@@ -64,7 +69,7 @@ export default class RotaryTunnel implements P5Sketch {
   private orbitAroundPoint = () => {};
 
   private updateRotationDelta = () => {
-    deltaRotation += this.rotationVelocity.currentValue;
+    deltaRotation += this.rotationVelocity.currentValue * 0.1;
   };
 
   easeInto = easeInto;
