8a1857041ce7339b707ec4a45d7ed4cb758937e2
Jose Conchello <conc2304@users.noreply.github.com>
2025-02-19 11:02:48 -0800

    i update sketch filenames

--- src/js/services/SketchRegistration.ts
+++ src/js/services/SketchRegistration.ts
@@ -1,10 +1,9 @@
-
 // Sketches
-import CenterWave from '@/js/sketches/SketchConstructorCenterWave';
-import WEBGLWave from '@/js/sketches/SketchConstructorWEBGLWave';
-import OuterWaves from '@/js/sketches/SketchConstructorOuterWaves';
+import CenterWave from '@/js/sketches/CenterOscillator.sketch';
+import WEBGLWave from '@/js/sketches/3DOscillator.sketch';
+import OuterWaves from '@/js/sketches/OuterOscillator.sketch';
 import Parametric from '@/js/sketches/ParametricCurve.sketch';
-import Tunnel from '@/js/sketches/SketchConstructorTunnel';
+import Tunnel from '@/js/sketches/Tunnel.sketch';
 // import BodyBrush from '@/js/sketches/SketchConstructorBodyBrush'; TODO - FIX
 import ParticleGrid from '../sketches/ParticleGrid.sketch';
 import { P5Sketch } from '../interfaces/P5Sketch.interface';
@@ -32,7 +31,7 @@ while (i < 3) {
   if (!SketchCatalog[i]) break;
 
   const { classConstructor } = SketchCatalog[i].catalogueInfo;
-  const newSketch =  new classConstructor()
+  const newSketch = new classConstructor();
   RegisteredSketches.push(newSketch);
   i++;
 }
